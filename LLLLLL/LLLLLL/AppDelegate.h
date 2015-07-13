0c1d2977e41d2e5dd451bc819e40865edafdfba4
lanou3g <794422136@qq.com>
2015-07-13 15:06:31 +0800

    New  Code <7.13:15>

---
+++ LLLLLL/LLLLLL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLLLLL
+//
+//  Created by lanou3g on 15/7/13.
+//  Copyright (c) 2015年 lanou3g.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
